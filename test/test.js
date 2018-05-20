d9f921a41b5fec2867cf3d5be414fb3684bb4560
unknown <anxiety.pb@gmail.com>
2018-05-20 17:08:00 +0200

    feat: add player color conversion

--- test/test.js
+++ test/test.js
@@ -11,14 +11,18 @@ describe('Replay parsing tests', () => {
     expect(test.teams['24']).to.be.an('array')
     expect(test.teams['24']).to.have.lengthOf(4)
     expect(test.players['4'].name).to.equal('S.o.K.o.L')
+    expect(test.players['4'].detectedRace).to.equal('O')
+    expect(test.players['4'].color).to.equal('#50c878')
     expect(test.players['6'].name).to.equal('Stormhoof')
+    expect(test.players['6'].detectedRace).to.equal('O')
+    expect(test.players['6'].color).to.equal('#800000')
   })
 
   it('parses a netease replay properly', () => {
     const test = new W3GReplay(`./replays/netease1.nwg`)
-
     expect(test.header.magic).to.equal('Warcraft III recorded game\u001a')
     expect(test.teams['24']).to.be.an('array')
     expect(test.players['3'].name).to.equal('rudan')
+    expect(test.players['3'].color).to.equal('#3eb489')
   })
 })
