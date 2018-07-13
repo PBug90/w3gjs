a9a6c262593de28623bf56d4e2f96428a97c3460
unknown <anxiety.pb@gmail.com>
2018-07-14 00:56:43 +0200

    improvement: example.js outputs resulting json byte size

--- example.js
+++ example.js
@@ -2,7 +2,16 @@ const fs = require('fs')
 const replays = fs.readdirSync('./replays/')
 const W3GReplay = require('.')
 
+function formatBytes(bytes,decimals) {
+   if(bytes == 0) return '0 Bytes';
+   var k = 1024,
+       dm = decimals || 2,
+       sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'],
+       i = Math.floor(Math.log(bytes) / Math.log(k));
+   return parseFloat((bytes / Math.pow(k, i)).toFixed(dm)) + ' ' + sizes[i];
+}
+
 replays.forEach((replayFile) => {
   const test = new W3GReplay(`./replays/${replayFile}`)
-  console.log(test)
+  console.log(formatBytes(Buffer.byteLength(JSON.stringify(test), 'utf8')))
 })
