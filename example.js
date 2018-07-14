44b0c7137cd2bfa5ae0a6b5838a0d12c705ba53c
unknown <anxiety.pb@gmail.com>
2018-07-14 12:33:08 +0200

    style: example.js conforms to standardjs

--- example.js
+++ example.js
@@ -3,17 +3,16 @@ const replays = fs.readdirSync('./replays/')
 const W3GReplay = require('.')
 const W3GParser = new W3GReplay()
 
-function formatBytes(bytes,decimals) {
-   if(bytes == 0) return '0 Bytes';
-   var k = 1024,
-       dm = decimals || 2,
-       sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'],
-       i = Math.floor(Math.log(bytes) / Math.log(k));
-   return parseFloat((bytes / Math.pow(k, i)).toFixed(dm)) + ' ' + sizes[i];
+function formatBytes (bytes, decimals) {
+  if (bytes === 0) return '0 Bytes'
+  const k = 1024
+  const dm = decimals || 2
+  const sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB']
+  const i = Math.floor(Math.log(bytes) / Math.log(k))
+  return parseFloat((bytes / Math.pow(k, i)).toFixed(dm)) + ' ' + sizes[i]
 }
 
 replays.forEach((replayFile) => {
-  const parser = new W3GReplay()
-  const parseResult = parser.parse(`./replays/${replayFile}`)
+  const parseResult = W3GParser.parse(`./replays/${replayFile}`)
   console.log(formatBytes(Buffer.byteLength(JSON.stringify(parseResult), 'utf8')))
 })
