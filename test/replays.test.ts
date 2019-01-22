792b9ef917b4312c039c4a23e5bad08694d65e1e
anxietypb <anxiety.pb@gmail.com>
2019-01-22 21:33:51 +0100

    test: add standard 1.30.4 replay file

--- test/replays.test.ts
+++ test/replays.test.ts
@@ -93,4 +93,10 @@ describe('Replay parsing tests', () => {
     expect(test.version).toBe('1.30.2+')
     expect(test.players.length).toBe(2)
   })  
+
+  it('parses a standard 1.30.4 replay properly', () => {
+    const test = Parser.parse(`./replays/standard_1304.w3g`)
+    expect(test.version).toBe('1.30.2+')
+    expect(test.players.length).toBe(2)
+  })    
 })
