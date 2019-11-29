4153bcc7843a0f67d47acff26302614975d24c09
Stannnnn <stann@live.nl>
2019-11-29 17:33:26 +0100

    Quotes

--- test/replays.test.ts
+++ test/replays.test.ts
@@ -170,10 +170,10 @@ describe('Replay parsing tests', () => {
         expect(test.players.length).toBe(2)
     })
 
-    it("parses a standard 1.30.4 replay properly as buffer", () => {
-        const buffer: Buffer = readFileSync("./replays/standard_1304.w3g")
+    it('parses a standard 1.30.4 replay properly as buffer', () => {
+        const buffer: Buffer = readFileSync('./replays/standard_1304.w3g')
         const test = Parser.parse(buffer)
-        expect(test.version).toBe("1.30.2+")
+        expect(test.version).toBe('1.30.2+')
         expect(test.players.length).toBe(2)
     })
 
