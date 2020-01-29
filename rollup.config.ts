cdf5e302f232eae93061ac5dc5db83d5724bbc58
anxietypb <anxiety.pb@gmail.com>
2020-01-29 21:29:17 +0100

    chore: update dependencies

--- rollup.config.ts
+++ rollup.config.ts
@@ -10,35 +10,35 @@ import builtins from 'rollup-plugin-node-builtins'
 const pkg = require('./package.json')
 
 export default {
-  input: `src/index.ts`,
-  output: [
-    { file: pkg.main, name: camelCase('index'), format: 'umd', sourcemap: true },
-    { file: pkg.module, format: 'es', sourcemap: true },
-  ],
-  // Indicate here external modules you don't wanna include in your bundle (i.e.: 'lodash')
-  external: [],
-  watch: {
-    include: 'src/**',
-  },
-  plugins: [
+    input: 'src/index.ts',
+    output: [
+        { file: pkg.main, name: camelCase('index'), format: 'umd', sourcemap: true },
+        { file: pkg.module, format: 'es', sourcemap: true }
+    ],
+    // Indicate here external modules you don't wanna include in your bundle (i.e.: 'lodash')
+    external: [],
+    watch: {
+        include: 'src/**'
+    },
+    plugins: [
     // Allow json resolution
-    json(),
-    // Compile TypeScript files
-    typescript({ useTsconfigDeclarationDir: true }),
-    // Allow bundling cjs modules (unlike webpack, rollup doesn't understand cjs)
-    commonjs(),
-    // Allow node_modules resolution, so you can use 'external' to control
-    // which external modules to include in the bundle
-    // https://github.com/rollup/rollup-plugin-node-resolve#usage
-    resolve(),
+        json(),
+        // Compile TypeScript files
+        typescript({ useTsconfigDeclarationDir: true, clean: true }),
+        // Allow bundling cjs modules (unlike webpack, rollup doesn't understand cjs)
+        commonjs(),
+        // Allow node_modules resolution, so you can use 'external' to control
+        // which external modules to include in the bundle
+        // https://github.com/rollup/rollup-plugin-node-resolve#usage
+        resolve(),
 
-    // Resolve source maps to the original source
-    sourceMaps(),
-    // Node globals
-    globals(),
-    // Node built-ins
-    // Causing Circular dependency warning
-    // https://github.com/calvinmetcalf/rollup-plugin-node-builtins/issues/39
-    builtins(),
-  ],
+        // Resolve source maps to the original source
+        sourceMaps(),
+        // Node globals
+        globals(),
+        // Node built-ins
+        // Causing Circular dependency warning
+        // https://github.com/calvinmetcalf/rollup-plugin-node-builtins/issues/39
+        builtins()
+    ]
 }
