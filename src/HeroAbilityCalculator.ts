c3fd7c0eb482ef70d1ad1231bcb29f03de9f9b73
pbug90 <pbug90@protonmail.com>
2021-02-24 21:14:18 +0100

    improvement: HeroAbilityCalculator implementation

--- src/HeroAbilityCalculator.ts
+++ src/HeroAbilityCalculator.ts
@@ -1,7 +1,29 @@
 import { Ability, Retraining } from "./Player";
 
+const ultimates = new Set<string>().add("AEtq");
+
 export function HeroAbilityCalculator(
   abilityOrder: (Ability | Retraining)[]
 ): { [key: string]: number } {
-  return {};
+  let abilities: { [key: string]: number } = {};
+  for (const ability of abilityOrder) {
+    if (ability.type === "ability") {
+      if (ultimates.has(ability.value) && abilities[ability.value] === 1) {
+        continue;
+      }
+      abilities[ability.value] = abilities[ability.value] || 0;
+      if (abilities[ability.value] < 3) {
+        abilities[ability.value]++;
+      }
+    }
+    if (ability.type === "retraining") {
+      abilities = {};
+    }
+  }
+  const herolevel = Object.values(abilities).reduce(
+    (previousValue, current) => previousValue + current,
+    0
+  );
+  console.log("Detected hero level " + herolevel);
+  return abilities;
 }
