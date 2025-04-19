d88541102e4103f6431c3c0fde71d5bf12eb2f72
Bogdan Gligorijević <bogdangligorijevic3@gmail.com>
2025-04-19 13:21:20 +0200

    Expose GameDataParser

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,9 @@
 import W3GReplay from "./W3GReplay";
 
+import GameDataParser from "./parsers/GameDataParser";
 import MetadataParser from "./parsers/MetadataParser";
 import RawParser from "./parsers/RawParser";
 import ReplayParser from "./parsers/ReplayParser";
 
 export default W3GReplay;
-export { MetadataParser, RawParser, ReplayParser };
+export { GameDataParser, MetadataParser, RawParser, ReplayParser };
