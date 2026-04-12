a4989e44010d5b121c63faa183af905b8377c7d6
unknown <pbug90@protonmail.com>
2026-04-12 20:48:21 +0200

    fix: ParserOutput is exported from entrypoint

--- src/index.ts
+++ src/index.ts
@@ -7,4 +7,4 @@ export * from "./parsers/RawParser.js";
 export * from "./parsers/ReplayParser.js";
 export * from "./parsers/ActionParser.js";
 export * from "./W3GReplay.js";
-export { type ItemID, Race } from "./types.js";
+export { type ItemID, Race, ParserOutput } from "./types.js";
